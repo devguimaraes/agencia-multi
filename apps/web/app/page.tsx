f1f2443d1a9dab476c51337497ace5fc7516423d
devguimaraes <devgmrs@gmail.com>
2026-02-27 01:18:24 -0300

    feat(home): remove seção de destaques do blog
    - Remove importação de BlogHighlights e lógica de fetch de posts
    
    - Remove componente BlogHighlights do layout da página inicial

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,3 @@
-import { BlogHighlights } from "@/components/marketing/BlogHighlights";
 import { FinalCTA } from "@/components/marketing/FinalCTA";
 import { GoogleReviewsSection } from "@/components/marketing/GoogleReviewsSection";
 import { HeroSection } from "@/components/marketing/HeroSection";
@@ -7,20 +6,8 @@ import { PortfolioHighlights } from "@/components/marketing/PortfolioHighlights"
 import { ServicesOverview } from "@/components/marketing/ServicesOverview";
 import { SomosDoRio } from "@/components/marketing/SomosDoRio";
 import { TrustStrip } from "@/components/marketing/TrustStrip";
-import { getAllPosts } from "@/lib/blog";
 
 export default async function Home() {
-  const allPosts = await getAllPosts();
-
-  // Sort posts by date (descending) and take the top 3
-  const recentPosts = allPosts
-    .sort((a, b) => {
-      const dateA = new Date(a.date.split("/").reverse().join("-")).getTime();
-      const dateB = new Date(b.date.split("/").reverse().join("-")).getTime();
-      return dateB - dateA;
-    })
-    .slice(0, 3);
-
   return (
     <>
       <HeroSection />
@@ -30,7 +17,6 @@ export default async function Home() {
       <PortfolioHighlights />
       <SomosDoRio />
       <GoogleReviewsSection />
-      <BlogHighlights posts={recentPosts} />
       <FinalCTA />
     </>
   );
